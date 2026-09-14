f9b04f1eab73107f3665cee3d5d566ee548e03a1
John Matthew Victorino <johnmatthew.victorino@proton.me>
2026-09-14 16:53:55 +0800

    feat(main): add `PizzaService` into DI container

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
+using test_api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSingleton(new PizzaService());
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
